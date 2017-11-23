b5e9615d8c4ba7eae2e458aec2d8f4421fc948a9
Timon Schubert <timon.schubert@sap.com>
2017-11-23 12:18:15 +0100

    Add sketch for determening MAC address of NodeMCU

---
+++ 2_Getting_started_with_IoT_electronics/6_Wifi/MAC_address/MAC_address.ino
@@ -0,0 +1,24 @@
+#include <ESP8266WiFi.h>
+
+const int mac_array_size = 6;
+uint8_t MAC_array[mac_array_size];
+char MAC_char[18];
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println();
+
+    WiFi.macAddress(MAC_array);
+    for (int i = 0; i < mac_array_size; ++i){
+      sprintf(MAC_char,"%s%02x:",MAC_char,MAC_array[i]);
+    }
+    MAC_char[17] = '\0';
+
+    Serial.print("Device MAC address is [");
+    Serial.print(MAC_char);
+    Serial.println("]");
+}
+
+void loop() {
+  
+} 
